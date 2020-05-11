1be7289b49dd7fc6164cf60c26d7c5b1c58b9816
michellibrito <michellidibrito@gmail.com>
2020-05-11 18:43:29 -0300

    alterando versão angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,11 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatCardModule, MatDividerModule, MatChipsModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import {MatCardModule } from '@angular/material/card';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatChipsModule } from '@angular/material/chips';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
